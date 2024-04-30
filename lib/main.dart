d587544498361a02c46bcb951c8bc5a7bae84cdd
Dan Chevalier <chevalier.dan@gmail.com>
2024-04-30 11:26:36 -0400

    checkbox with tap

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
-import 'dart:convert';
-import 'dart:io';
 import 'dart:math';
 
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+
 import './lorem.dart' as lorem;
 import 'package:flutter/gestures.dart';
 import 'package:two_dimensional_scrollables/two_dimensional_scrollables.dart';
+
+/// A builder that includes an Offset to draw the context menu at.
+typedef ContextMenuBuilder = Widget Function(
+    BuildContext context, Offset offset);
 
 class LogData {
   LogData._(this.about, this.address, this.date);
@@ -134,9 +139,39 @@ Size _textSize(String text, TextStyle style) {
 class _TableExampleState extends State<TableExample> {
   late final ScrollController _verticalController = ScrollController();
   late final ScrollController _horizontalController = ScrollController();
+  final Set<int> selections = <int>{};
   double maxWidth = 0.0;
   //TODO: may need to rebuild the table at the current scroll position when we get sections that are longer?
   // We would need to min width to the current screen size
+
+  @override
+  void initState() {
+    super.initState();
+    // On web, disable the browser's context menu since this example uses a custom
+    // Flutter-rendered context menu.
+    if (kIsWeb) {
+      BrowserContextMenu.disableContextMenu();
+    }
+  }
+
+  @override
+  void dispose() {
+    if (kIsWeb) {
+      BrowserContextMenu.enableContextMenu();
+    }
+    super.dispose();
+  }
+
+  void _showDialog(BuildContext context) {
+    Navigator.of(context).push(
+      DialogRoute<void>(
+        context: context,
+        builder: (BuildContext context) =>
+            const AlertDialog(title: Text('You clicked print!')),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final style = Theme.of(context).textTheme.bodyMedium?.copyWith(
@@ -179,55 +214,57 @@ class _TableExampleState extends State<TableExample> {
               ],
             ),
             Expanded(
-              child: Scrollbar(
-                controller: _verticalController,
+              child: _ContextMenuRegion(
+                contextMenuBuilder: (context, offset) {
+                  // The custom context menu will look like the default context menu
+                  // on the current platform with a single 'Print' button.
+                  return AdaptiveTextSelectionToolbar.buttonItems(
+                    anchors: TextSelectionToolbarAnchors(
+                      primaryAnchor: offset,
+                    ),
+                    buttonItems: <ContextMenuButtonItem>[
+                      ContextMenuButtonItem(
+                        onPressed: () {
+                          ContextMenuController.removeAny();
+                          _showDialog(context);
+                        },
+                        label: 'Copy Selected Rows',
+                      ),
+                      ContextMenuButtonItem(
+                        onPressed: () {
+                          ContextMenuController.removeAny();
+                          _showDialog(context);
+                        },
+                        label: 'Copy Selected Rows with Metadata',
+                      ),
+                      ContextMenuButtonItem(
+                        onPressed: () {
+                          ContextMenuController.removeAny();
+                          _showDialog(context);
+                        },
+                        label: 'Hide items with same Address',
+                      ),
+                      ContextMenuButtonItem(
+                        onPressed: () {
+                          ContextMenuController.removeAny();
+                          _showDialog(context);
+                        },
+                        label: 'Show items with same Address',
+                      ),
+                    ],
+                  );
+                },
                 child: Scrollbar(
-                  controller: _horizontalController,
-                  child: SelectionArea(
-                    contextMenuBuilder: (context, selectableRegionState) {
-                      final List<ContextMenuButtonItem> buttonItems =
-                          selectableRegionState.contextMenuButtonItems;
-                      buttonItems.insert(
-                        0,
-                        ContextMenuButtonItem(
-                          label: 'Copy Logs',
-                          onPressed: () {},
-                        ),
-                      );
-                      buttonItems.insert(
-                        0,
-                        ContextMenuButtonItem(
-                          label: 'Copy Logs with Metadata',
-                          onPressed: () {},
-                        ),
-                      );
-                      buttonItems.insert(
-                        0,
-                        ContextMenuButtonItem(
-                          label: 'Copy Address',
-                          onPressed: () {},
-                        ),
-                      );
-                      buttonItems.insert(
-                        0,
-                        ContextMenuButtonItem(
-                          label: 'Copy Date',
-                          onPressed: () {},
-                        ),
-                      );
-
-                      return AdaptiveTextSelectionToolbar.buttonItems(
-                        anchors: selectableRegionState.contextMenuAnchors,
-                        buttonItems: buttonItems,
-                      );
-                    },
+                  controller: _verticalController,
+                  child: Scrollbar(
+                    controller: _horizontalController,
                     child: TableView.builder(
                       verticalDetails: ScrollableDetails.vertical(
                           controller: _verticalController),
                       horizontalDetails: ScrollableDetails.horizontal(
                           controller: _horizontalController),
                       cellBuilder: _buildCell,
-                      columnCount: 1,
+                      columnCount: 2,
                       columnBuilder: _buildColumnSpan,
                       rowCount: widget.items.length,
                       rowBuilder: _buildRowSpan,
@@ -259,28 +296,51 @@ class _TableExampleState extends State<TableExample> {
 
   TableViewCell _buildCell(BuildContext context, TableVicinity vicinity) {
     var index = vicinity.row;
+    var isSelected = selections.contains(index);
     var row = widget.items[index];
+
+    if (vicinity.column == 0) {
+      if (row.showMetadata && row.isLastLine) {
+        return TableViewCell(
+          child: Container(),
+        );
+      } else {
+        return TableViewCell(
+          child: Checkbox(
+              value: isSelected,
+              onChanged: (_) {
+                setState(() {
+                  if (isSelected) {
+                    selections.remove(index);
+                  } else {
+                    selections.add(index);
+                  }
+                });
+              }),
+        );
+      }
+    }
     Widget rowContents;
     if (row.showMetadata && row.isLastLine) {
       rowContents = ListTile(
-        title: SelectionContainer.disabled(
-          child: Row(
-            children: [
-              RichText(
-                  text: TextSpan(
-                text: 'Address: ${row.data.address}',
-                style: const TextStyle(fontStyle: FontStyle.italic),
-              )),
-              SizedBox(
-                width: 20.0,
-              ),
-              RichText(
-                  text: TextSpan(
-                text: 'Date: ${row.data.date}',
-                style: const TextStyle(fontStyle: FontStyle.italic),
-              )),
-            ],
-          ),
+        title: Row(
+          children: [
+            RichText(
+                text: TextSpan(
+              text: 'Address: ${row.data.address}',
+              style: const TextStyle(
+                  fontStyle: FontStyle.italic, color: Colors.grey),
+            )),
+            SizedBox(
+              width: 20.0,
+            ),
+            RichText(
+                text: TextSpan(
+              text: 'Date: ${row.data.date}',
+              style: const TextStyle(
+                  fontStyle: FontStyle.italic, color: Colors.grey),
+            )),
+          ],
         ),
       );
     } else {
@@ -308,7 +368,7 @@ class _TableExampleState extends State<TableExample> {
 
     return TableSpan(
       foregroundDecoration: decoration,
-      extent: FixedTableSpanExtent(maxWidth),
+      extent: FixedTableSpanExtent(index == 0 ? 25.0 : maxWidth),
       onEnter: (_) => print('Entered column $index'),
       recognizerFactories: <Type, GestureRecognizerFactory>{
         TapGestureRecognizer:
@@ -333,9 +393,106 @@ class _TableExampleState extends State<TableExample> {
         TapGestureRecognizer:
             GestureRecognizerFactoryWithHandlers<TapGestureRecognizer>(
           () => TapGestureRecognizer(),
-          (TapGestureRecognizer t) => t.onTap = () => print('Tap row $index'),
+          (TapGestureRecognizer t) => t.onTap = () {
+            setState(() {
+              selections.contains(index)
+                  ? selections.remove(index)
+                  : selections.add(index);
+            });
+          },
         ),
       },
+    );
+  }
+}
+
+/// Shows and hides the context menu based on user gestures.
+///
+/// By default, shows the menu on right clicks and long presses.
+class _ContextMenuRegion extends StatefulWidget {
+  /// Creates an instance of [_ContextMenuRegion].
+  const _ContextMenuRegion({
+    required this.child,
+    required this.contextMenuBuilder,
+  });
+
+  /// Builds the context menu.
+  final ContextMenuBuilder contextMenuBuilder;
+
+  /// The child widget that will be listened to for gestures.
+  final Widget child;
+
+  @override
+  State<_ContextMenuRegion> createState() => _ContextMenuRegionState();
+}
+
+class _ContextMenuRegionState extends State<_ContextMenuRegion> {
+  Offset? _longPressOffset;
+
+  final ContextMenuController _contextMenuController = ContextMenuController();
+
+  static bool get _longPressEnabled {
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+      case TargetPlatform.iOS:
+        return true;
+      case TargetPlatform.macOS:
+      case TargetPlatform.fuchsia:
+      case TargetPlatform.linux:
+      case TargetPlatform.windows:
+        return false;
+    }
+  }
+
+  void _onSecondaryTapUp(TapUpDetails details) {
+    _show(details.globalPosition);
+  }
+
+  void _onTap() {
+    if (!_contextMenuController.isShown) {
+      return;
+    }
+    _hide();
+  }
+
+  void _onLongPressStart(LongPressStartDetails details) {
+    _longPressOffset = details.globalPosition;
+  }
+
+  void _onLongPress() {
+    assert(_longPressOffset != null);
+    _show(_longPressOffset!);
+    _longPressOffset = null;
+  }
+
+  void _show(Offset position) {
+    _contextMenuController.show(
+      context: context,
+      contextMenuBuilder: (BuildContext context) {
+        return widget.contextMenuBuilder(context, position);
+      },
+    );
+  }
+
+  void _hide() {
+    _contextMenuController.remove();
+  }
+
+  @override
+  void dispose() {
+    _hide();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onSecondaryTapUp: _onSecondaryTapUp,
+      onTap: _onTap,
+      onLongPress: _longPressEnabled ? _onLongPress : null,
+      onLongPressStart: _longPressEnabled ? _onLongPressStart : null,
+      child: widget.child,
     );
   }
 }
